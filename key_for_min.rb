c469e5b6874d6be5a9d4be00edc3869bd47dd849
Ebony <ebony.furr@yahoo.com>
2020-10-29 19:12:36 -0500

    commit tree db0e4203a708ba370c719090e6e416362c33e41e

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    lowest_value = Float::INFINITY
+    low_key = ""
+    name_hash.each do |key, value|
+        if value < lowest_value
+            lowest_value = value
+            low_key = key
+        end
+    end
+    return low_key if !name_hash.empty?
 end
